7274cdb08b78ddaa3430f60b5174157d8d7e5ff5
Rohit Bhadani <bhadanirohit1@gmail.com>
2020-08-20 10:44:47 +0530

    some tets performing

--- bin/copy-cloudinary-plugin.sh
+++ bin/copy-cloudinary-plugin.sh
@@ -1,3 +1,2 @@
 mkdir -p /app/content/adapters/storage/ghost-cloudinary-store
-cp -r node_modules/ghost-cloudinary-store /app/node_modules/ghost/core/server/adapters/storage
 cp -r node_modules/ghost-cloudinary-store /app/content/adapters/storage/
