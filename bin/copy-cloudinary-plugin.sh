2b356392c229accd65dafb37614b8baf7d904c9a
Rohit Bhadani <bhadanirohit1@gmail.com>
2020-08-20 10:40:39 +0530

    path updated

--- bin/copy-cloudinary-plugin.sh
+++ bin/copy-cloudinary-plugin.sh
@@ -1,3 +1,3 @@
-mkdir -p content/adapters/storage/ghost-cloudinary-store
-cp -r node_modules/ghost-cloudinary-store node_modules/ghost/core/server/adapters/storage
-cp -r node_modules/ghost-cloudinary-store content/adapters/storage/
+mkdir -p /app/content/adapters/storage/ghost-cloudinary-store
+cp -r node_modules/ghost-cloudinary-store /app/node_modules/ghost/core/server/adapters/storage
+cp -r node_modules/ghost-cloudinary-store /app/content/adapters/storage/
