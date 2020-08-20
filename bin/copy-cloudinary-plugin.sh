f787c5b364d9b7224d53fef6e217ade049c34461
Rohit Bhadani <bhadanirohit1@gmail.com>
2020-08-20 14:01:06 +0530

    made mv commnaf silent

--- bin/copy-cloudinary-plugin.sh
+++ bin/copy-cloudinary-plugin.sh
@@ -1,1 +1,1 @@
-mv node_modules/ghost-storage-cloudinary node_modules/ghost/core/server/adapters/storage
+mv node_modules/ghost-storage-cloudinary node_modules/ghost/core/server/adapters/storage 2>/dev/null
