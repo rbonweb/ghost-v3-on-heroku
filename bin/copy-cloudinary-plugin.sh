d02731d680ed02d2fe4b7c99a4cc51bc4124e1c9
Rohit Bhadani <bhadanirohit1@gmail.com>
2020-08-20 10:38:18 +0530

    bug fixed to check

---
+++ bin/copy-cloudinary-plugin.sh
@@ -0,0 +1,3 @@
+mkdir -p content/adapters/storage/ghost-cloudinary-store
+cp -r node_modules/ghost-cloudinary-store node_modules/ghost/core/server/adapters/storage
+cp -r node_modules/ghost-cloudinary-store content/adapters/storage/
